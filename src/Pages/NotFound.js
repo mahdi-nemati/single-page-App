764f2ad7bedd4b46fc7889a7e935aedf8de7444d
mahdi <this.nemati@gmail.com>
2022-01-03 17:01:10 +0330

    third

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -3,12 +3,18 @@ import Layout from "../Layout/Layout";
 const NotFound = () => {
   return (
     <Layout>
-      <p>
-        404
-        <br />
-        Not Found
-      </p>
-      <Link to="/">Home</Link>
+      <section className="homePage">
+        <header className="homePageHeader">
+          <p>
+            404
+            
+          </p>
+        </header>
+        <body className="notBody">
+          <p>صفحه ای که دنبالش بودید پیدا نشد</p>
+          <Link to="/">Home</Link>
+        </body>
+      </section>
     </Layout>
   );
 };
